ebb42f91d7fe712fa7f3e810712a7f3233f30b76
Andrew MacLachlan <51865303+andrewmaclachlan@users.noreply.github.com>
2020-02-21 21:59:47 +0000

    travis

--- _deploy.sh
+++ _deploy.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+# configure your name and email if you have not done so
+git config --global user.email "a.maclachlan@ucl.ac.uk"
+git config --global user.name "andrewmaclachlan"
 
-set -e
-
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
-
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
 cd book-output
+git rm -rf *
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m"Update the book"
 git push -q origin gh-pages
